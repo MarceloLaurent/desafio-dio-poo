7bafe21dc8b799fed62fcba4185077858c82d723
Marcelo <marcelo-laurentino@hotmail.com.br>
2022-11-19 20:21:05 -0300

    Bootcamp e Dev

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -20,8 +22,32 @@ public class Main {
         mentoria.setDescricao("Primeiros passos com a Lingueagem JAVA");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Formação Java Developer");
+        bootcamp.setDescricao("Aprendendo a Linguagem Java do nível básico ao avançado");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devMarcelo = new Dev();
+        devMarcelo.setNome("Marcelo");
+        devMarcelo.inscreverBootcamp(bootcamp);
+        devMarcelo.progredir();
+        devMarcelo.progredir();
+        System.out.println("Conteúdos Inscritos Marcelo: " + devMarcelo.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Marcelo: " + devMarcelo.getConteudosConcluidos());
+        System.out.println("XP: " + devMarcelo.calcularTotalXp());
+
+        Dev devLaurentino = new Dev();
+        devLaurentino.setNome("Laurentino");
+        devLaurentino.inscreverBootcamp(bootcamp);
+        devLaurentino.progredir();
+        System.out.println("Conteúdos Inscritos Laurentino: " + devLaurentino.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Laurentino: " + devLaurentino.getConteudosConcluidos());
+        System.out.println("XP: " + devLaurentino.calcularTotalXp());
     }
 }
